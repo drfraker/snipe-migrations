a19309f5e9e004699c749fb4aa394bd5985f3607
Jack Webb-Heller <jack@jackwebbheller.com>
2019-03-17 13:01:21 +0000

    Neatened up config file and added new settings for database seeding

--- config/snipe.php
+++ config/snipe.php
@@ -1,6 +1,28 @@
 <?php
 
 return [
-    'snapshot-location' => __DIR__.'/../snapshots/snipe_snapshot.sql',
-    'snipefile-location' => __DIR__.'/../snapshots/.snipe',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Storage Locations
+    |--------------------------------------------------------------------------
+    | By default, SnipeMigrations will store snipe files and database snapshots
+    | in /vendor/drfraker/snipe-migrations/snapshots. If you would like to
+    | change the location of the files, update the paths below.
+    */
+    'snapshot-location'  => base_path('vendor/drfraker/snipe-migrations/snapshots/') . 'snipe_snapshot.sql',
+    'snipefile-location' => base_path('vendor/drfraker/snipe-migrations/snapshots/') . '.snipe',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Database Seeding
+    |--------------------------------------------------------------------------
+    | By default SnipeMigrations will refresh the database, run all migrations,
+    | and start each test with an empty database. If you would like to seed
+    | the database after refreshing it, enable the setting below. A custom
+    | class can be set, otherwise, the default DatabaseSeeder will run.
+    */
+    'seed-database'      => false,
+    'seed-class'         => 'DatabaseSeeder',
+
 ];
