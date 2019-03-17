e35e42cd8bc5ee35d381fa00d7e956381da85023
Jack Webb-Heller <jack@jackwebbheller.com>
2019-03-17 13:01:42 +0000

    Neatened up SnipeClearCommand and removed error suppression

--- src/SnipeClearCommand.php
+++ src/SnipeClearCommand.php
@@ -6,11 +6,33 @@ use Illuminate\Console\Command;
 
 class SnipeClearCommand extends Command
 {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
     protected $signature = 'snipe:clear';
 
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
     public function handle()
     {
-        @unlink(config('snipe.snipefile-location'));
-        $this->info('Cleared snipe migration snapshot.');
+        $snipefile = config('snipe.snipefile-location');
+
+        if ($snipefile && file_exists($snipefile)) {
+            try {
+                unlink($snipefile);
+                $this->info('Cleared snipe migration snapshot.');
+            } catch (\Exception $exception) {
+                $this->warn("Could not delete snipe migration file: {$exception->getMessage()}");
+            }
+
+            return;
+        }
+
+        $this->info('No Snipe migration snapshot found (it may have been cleared already).');
     }
 }
