70dc1e013906b86d87fb40b26886c5ee2f4898e8
spaceemotion <spaceemotion@users.noreply.github.com>
2019-09-28 01:39:53 +0200

    Add support for custom binary paths (#29)
    * Add support for custom binary paths
    
    Users can now configure the exact path to the mysql and mysqldump binaries for different kind of set ups.
    
    * Don't typehint the execute() method just yet
    
    * Fix style in snipe config
    
    * Add proper snipe test support
    
    These changes now properly reset the application state before each test run, as well as adds a few helpful methods to deal with snipe internals.
    
    * Add test for checking for the migrate:fresh command
    
    * Add test to detect and reflect database changes
    
    * Fix style issues in SnipeMigrationsTest
    
    * Add test for custom binary prefixes
    
    This uses a mockery extension package to overwrite the native “exec” method with a customizable mock.
    
    * Fix style issues in SnipeMigrationsTest
    
    * Add back the snapshots folder
    
    I seem to have deleted this by mistake

--- tests/SnipeMigrationsTest.php
+++ tests/SnipeMigrationsTest.php
@@ -2,20 +2,57 @@
 
 namespace Drfraker\SnipeMigrations\Tests;
 
+use phpmock\mockery\PHPMockery;
 use Drfraker\SnipeMigrations\Snipe;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Artisan;
+use Drfraker\SnipeMigrations\SnipeDatabaseState;
 
 class SnipeMigrationsTest extends TestCase
 {
+    /** @var Snipe */
     protected $snipe;
+
+    /**
+     * The absolute path to the "snapshots" folder.
+     * @var string
+     */
+    protected $snipeFolder;
+
+    /**
+     * The full path to the snipe_snapshot.sql file.
+     * @var string
+     */
+    protected $snapshotFile;
+
+    /**
+     * The full path to the .snip file.
+     * @var string
+     */
+    protected $snipeFile;
 
     public function setUp() :void
     {
         parent::setUp();
 
+        // This folder will reside in the orchestra sandbox environment
+        // placed at vendor/orchestra/testbench-core/laravel
+        $this->snipeFolder = base_path('vendor/drfraker/snipe-migrations/snapshots');
+
+        $this->snapshotFile = config('snipe.snapshot-location');
+        $this->snipeFile = config('snipe.snipefile-location');
+
+        // Reset state before each run
         $this->clearSnapshotDir();
+        $this->clearMigrationsDir();
+        $this->resetDatabaseState();
 
         $this->snipe = new Snipe();
+
+        // Add support for native method mocking. We pre-define the exec method here.
+        // If we would just call the mock method in our tests where we need it, PHP
+        // would have already cached the call to the native method instead.
+        PHPMockery::define('Drfraker\SnipeMigrations', 'exec');
     }
 
     /** @test */
@@ -23,28 +60,101 @@ class SnipeMigrationsTest extends TestCase
     {
         $this->mimicInMemoryDatabase();
 
-        Artisan::ShouldReceive('call')->never();
+        Artisan::shouldReceive('call')->never();
+
+        $this->snipe->importSnapshot();
+    }
+
+    /** @test */
+    public function it_calls_migration_commands_for_mysql_databases()
+    {
+        Artisan::shouldReceive('call')->with('migrate:fresh');
+
+        $this->snipe->importSnapshot();
+    }
+
+    /** @test */
+    public function it_detects_file_changes_in_the_migration_folder()
+    {
+        Artisan::shouldReceive('call');
+
+        // The first time we run snipe, we have no migrations
+        $this->snipe->importSnapshot();
+
+        $this->assertFileExists($this->snipeFile);
+        $this->assertEquals(0, file_get_contents($this->snipeFile));
+
+        $this->copyDefaultMigrations();
+
+        // Let's do a re-run
+        $this->resetDatabaseState();
+        $this->snipe->importSnapshot();
+
+        // This time the changes should have been picked up
+        $this->assertGreaterThan(0, file_get_contents($this->snipeFile));
+    }
+
+    /** @test */
+    public function it_allows_a_custom_prefix_for_executables()
+    {
+        Artisan::shouldReceive('call')->withAnyArgs();
+
+        // Define the custom binary path we want to use
+        static $customBinary = 'docker-compose exec db mysql';
+        config()->set(['snipe.binaries.mysqldump' => $customBinary]);
+
+        PHPMockery::mock('Drfraker\SnipeMigrations', 'exec')
+            ->withArgs(static function ($args) use ($customBinary) {
+                return strpos($args, $customBinary) === 0;
+            })->once();
 
         $this->snipe->importSnapshot();
     }
 
     protected function mimicInMemoryDatabase(): void
     {
-        config()->set('database.default', 'sqlite');
-        config()->set('database.connections.sqlite.database', ':memory:');
+        config()->set([
+            'database.default' => 'sqlite',
+            'database.connections.sqlite.database' => ':memory:',
+        ]);
     }
 
     protected function clearSnapshotDir()
     {
-        $snipefile = '../snapshots/.snipe';
-        $snapshot = '../snapshots/snipe_snapshot.sql';
+        if (! is_dir($this->snipeFolder)) {
+            mkdir($this->snipeFolder, 0777, true);
 
-        if (file_exists($snipefile)) {
-            unlink($snipefile);
+            return;
         }
 
-        if (file_exists($snapshot)) {
-            unlink($snapshot);
+        // Prepare sandbox for subsequent runs
+        if (file_exists($this->snipeFile)) {
+            $this->assertTrue(unlink($this->snipeFile));
         }
+
+        if (file_exists($this->snapshotFile)) {
+            $this->assertTrue(unlink($this->snapshotFile));
+        }
+    }
+
+    protected function clearMigrationsDir()
+    {
+        foreach (File::allFiles(database_path('migrations')) as $file) {
+            if ($file->getFilename() !== '.gitkeep') {
+                unlink($file->getRealPath());
+            }
+        }
+    }
+
+    protected function copyDefaultMigrations(): void
+    {
+        foreach (File::allFiles(base_path('migrations')) as $file) {
+            copy($file->getRealPath(), database_path("migrations/{$file->getFilename()}"));
+        }
+    }
+
+    protected function resetDatabaseState(): void
+    {
+        SnipeDatabaseState::$checkedForDatabaseFileChanges = false;
     }
 }
