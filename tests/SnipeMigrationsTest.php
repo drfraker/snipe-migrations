50a0001a2f1d7612b2cde5a489e3a5c3d46361f1
André Gräßel <hello@spaceemotion.net>
2019-09-27 11:24:52 +0200

    Add proper snipe test support
    These changes now properly reset the application state before each test run, as well as adds a few helpful methods to deal with snipe internals.

--- tests/SnipeMigrationsTest.php
+++ tests/SnipeMigrationsTest.php
@@ -3,17 +3,49 @@
 namespace Drfraker\SnipeMigrations\Tests;
 
 use Drfraker\SnipeMigrations\Snipe;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Artisan;
+use Drfraker\SnipeMigrations\SnipeDatabaseState;
 
 class SnipeMigrationsTest extends TestCase
 {
+    /** @var Snipe */
     protected $snipe;
+
+    /**
+     * The absolute path to the "snapshots" folder
+     * @var
+     */
+    protected $snipeFolder;
+
+    /**
+     * The full path to the snipe_snapshot.sql file
+     * @var string
+     */
+    protected $snapshotFile;
+
+    /**
+     * The full path to the .snip file
+     * @var string
+     */
+    protected $snipeFile;
+
 
     public function setUp() :void
     {
         parent::setUp();
 
+        // This folder will reside in the orchestra sandbox environment
+        // placed at vendor/orchestra/testbench-core/laravel
+        $this->snipeFolder = base_path('vendor/drfraker/snipe-migrations/snapshots');
+
+        $this->snapshotFile = config('snipe.snapshot-location');
+        $this->snipeFile = config('snipe.snipefile-location');
+
+        // Reset state before each run
         $this->clearSnapshotDir();
+        $this->clearMigrationsDir();
+        $this->resetDatabaseState();
 
         $this->snipe = new Snipe();
     }
@@ -23,28 +55,47 @@ class SnipeMigrationsTest extends TestCase
     {
         $this->mimicInMemoryDatabase();
 
-        Artisan::ShouldReceive('call')->never();
+        Artisan::shouldReceive('call')->never();
 
         $this->snipe->importSnapshot();
     }
 
     protected function mimicInMemoryDatabase(): void
     {
-        config()->set('database.default', 'sqlite');
-        config()->set('database.connections.sqlite.database', ':memory:');
+        config()->set([
+            'database.default' => 'sqlite',
+            'database.connections.sqlite.database' => ':memory:',
+        ]);
     }
 
     protected function clearSnapshotDir()
     {
-        $snipefile = '../snapshots/.snipe';
-        $snapshot = '../snapshots/snipe_snapshot.sql';
-
-        if (file_exists($snipefile)) {
-            unlink($snipefile);
+        if (!is_dir($this->snipeFolder)) {
+            mkdir($this->snipeFolder, 0777, true);
+            return;
         }
 
-        if (file_exists($snapshot)) {
-            unlink($snapshot);
+        // Prepare sandbox for subsequent runs
+        if (file_exists($this->snipeFile)) {
+            $this->assertTrue(unlink($this->snipeFile));
         }
+
+        if (file_exists($this->snapshotFile)) {
+            $this->assertTrue(unlink($this->snapshotFile));
+        }
+    }
+
+    protected function clearMigrationsDir()
+    {
+        foreach (File::allFiles(database_path('migrations')) as $file) {
+            if ($file->getFilename() !== '.gitkeep') {
+                unlink($file->getRealPath());
+            }
+        }
+    }
+
+    protected function resetDatabaseState(): void
+    {
+        SnipeDatabaseState::$checkedForDatabaseFileChanges = false;
     }
 }
